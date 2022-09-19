5e6352fe491d8d85051c7b0ca82d808cf21e9986
PiyumiDulakshi99 <113334805+PiyumiDulakshi99@users.noreply.github.com>
2022-09-19 23:09:26 +0530

    Update connect.sql

--- PickMediTime Project/connect.sql
+++ PickMediTime Project/connect.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE pick_medi_time;
 CREATE TABLE book_appoinment (
     your_name  varchar(100) NOT NULL,
-    your_index  int(10) NOT NULL,
-    your_contact_number  bigint(10) NOT NULL,
-    email varchar(50) NOT NULL,
-    date date NOT NULL,
+    your_index  varchar(100) NOT NULL,
+    your_contact_number  varchar(100) NOT NULL,
+    email varchar(100) NOT NULL,
+    date varchar(100) NOT NULL,
     PRIMARY KEY (your_index)
 );
