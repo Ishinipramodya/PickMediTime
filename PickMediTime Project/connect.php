70c2ef84dfbd200cff4676fb7c94da014d4a3c83
Ishinipramodya <ishinipramodya0@gmail.com>
2022-09-19 16:46:56 +0530

    Changed connect code

--- PickMediTime Project/connect.php
+++ PickMediTime Project/connect.php
@@ -1,21 +1,51 @@
 <?php
-$yourName=$_POST['yourName'];
-$yourIndex=$_POST['yourIndex'];
-$yourContactNumber=$_POST['yourContactNumber'];
-$email=$_POST['email'];
-$date=$_POST['date'];
+$yourName = $_POST['yourName'];
+$yourIndex = $_POST['yourIndex'];
+$yourContactNumber = $_POST['yourContactNumber'];
+$email = $_POST['email'];
+$date = $_POST['date'];
 
 //Database connection
-$conn = new mysqli('localhost','root','','pickmeditime');
-if($conn->connect_error){
-	die('Connection Failed :'.$conn->connect_error');
+if(!empty($yourName)|| !empty($yourIndex)|| !empty($yourContactNumber)|| !empty($email)|| !empty($date)){
+	$host = "localhost";
+	$dbUserName = "root";
+	$dbPassword = "";
+	$dbname = "youtube";
+
+	//create connection
+	$conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+
+	if(mysqli_connect_error()){
+		die('Connect Error('.mysqli_connect_errno().')'. mysqli_connect_error());
+
+	}else{
+		$SELECT = "SELECT email From bookappoinment Where email = ?Limit 1";
+		$INSERT = "INSERT Into bookappoinment(yourName, yourIndex, yourContactNumber, email, date) values(?, ?, ?, ?, ?)";
+
+		//Prepare statement
+		$stmt = $conn->prepare($SELECT);
+		$stmt->bind_param("s", $email);
+		$stmt->execute();
+		$stmt->bind_result($email);
+		$stmt->store_result();
+		$rnum = $stmt->num_rows;
+
+		if($rnum==0){
+			$stmt->close();
+
+			$stmt = $conn->prepare($INSERT);
+			$stmt->bind_param("ssssi", $yourName, $yourIndex, $yourContactNumber, $email, $date);
+			$stmt->execute();
+			echo "New record inserted successfully";
+		}else{
+			echo "Someone already registered using this email";
+		}
+		$stmt->close();
+		$conn->close();
+	}
+
 }else{
-	$stmt = $conn->prepare("insert into appoinment(yourName,yourIndex,yourContactNumber,email,date)
-		values(?,?,?,?,?)");
-	$stmt->bind_param("ssssi",$yourName,$yourIndex,$yourContactNumber,$email,$date);
-	$stmt->execute();
-	echo "registration successfully...";
-	$stmt->close();
-	$conn->close();	
+	echo "All field are required";
+	die();
 }
 ?>
